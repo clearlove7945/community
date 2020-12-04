584795520443896f334a417a0183871d50d9ef4b
clealove7945 <534149578@qq.com>
2020-12-04 18:23:45 +0800

    access token

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 ##码匠社区
 
 ##资料
-https://spring.io
-https://spring.io/guides/gs/serving-web-content/
-https://elasticsearch.cn
-https://docs.github.com/cn/free-pro-team@latest/github/authenticating-to-github/connecting-to-github-with-ssh
+[Spring 文档](https://spring.io)
+[Spring Web](https://spring.io/guides/gs/serving-web-content/)
+[es社区](https://elasticsearch.cn)
+[Github deploy key](https://developer.github.com/v3/guides/managing-deploy-keys/)
+[Bootstrap 文档](https://v3.bootcss.com)
+[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+
+
+
+
+
 
 ##工具
-https://git-scm.com
+[Git](https://git-scm.com)
+[visual paradigm](https://www.visual-paradigm.com/cn/)
+
+
+
+
+
+
+
+
---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,32 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.controller.dto.AccessTokenDTO;
+import life.majiang.community.controller.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("757ae7fb239700b5a47a");
+        accessTokenDTO.setClient_secret("dd06dc0e0c20036d18977312f5f5120d4ee7fbf1");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
