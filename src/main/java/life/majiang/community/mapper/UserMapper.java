e1c0b284c55a44cc63584a4816ee415a4e60aaf7
clealove7945 <534149578@qq.com>
2020-12-07 12:04:23 +0800

    首页发布列表

--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -13,4 +13,7 @@ public interface UserMapper {
 
     @Select("select * from USER where token = #{token}")
     User findByToken(@Param("token") String token);
+
+    @Select("select * from USER where id = #{id}")
+    User findById(@Param("id") Integer id);
 }
